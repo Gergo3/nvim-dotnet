c4cb3c215a0c289b3cd7c0e1bb7edf87ff182ee9
Simon Gergely <simon.gergely2006@gmail.com>
2025-12-06 19:23:27 +0100

    Move building out of nvim-dap-cs config

--- .config/nvim/dap/nvim-dotnet.lua
+++ .config/nvim/dap/nvim-dotnet.lua
@@ -1,14 +1,9 @@
 --debugger setup
-require('dap-cs').setup(
-    {
-        dap_configurations = {
-            {
-                preLaunchTask = function()
-                    print("Building project before debugging...")
-                    local result = vim.fn.system("dotnet build -c Debug")
-                    print(result)
-                end,
-            },
-        }
-    }
-)
+require('dap-cs').setup()
+
+-- Automatically build before debugging
+dap.listeners.before['event_initialized']['build_before_debug'] = function(session, body)
+    print("Building project before debugging...")
+    local result = vim.fn.system("dotnet build -c Debug")
+    print(result)
+end
