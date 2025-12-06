84587d31bcd9531cb594e09205519d460723ee2b
Simon Gergely <simon.gergely2006@gmail.com>
2025-12-06 19:04:14 +0100

    Fix lua syntax in dap setup

--- .config/nvim/dap/nvim-dotnet.lua
+++ .config/nvim/dap/nvim-dotnet.lua
@@ -1,12 +1,14 @@
 --debugger setup
 require('dap-cs').setup(
-    dap_configurations = {
-        {
-            preLaunchTask = function()
-                print("Building project before debugging...")
-                local result = vim.fn.system("dotnet build -c Debug")
-                print(result)
-            end,
-        },
+    {
+        dap_configurations = {
+            {
+                preLaunchTask = function()
+                    print("Building project before debugging...")
+                    local result = vim.fn.system("dotnet build -c Debug")
+                    print(result)
+                end,
+            },
+        }
     }
 )
