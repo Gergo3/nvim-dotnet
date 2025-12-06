96a85368fcd60913feb99f9c23b4740699f904be
Simon Gergely <simon.gergely2006@gmail.com>
2025-12-06 19:38:17 +0100

    Do not print build output

--- .config/nvim/dap/nvim-dotnet.lua
+++ .config/nvim/dap/nvim-dotnet.lua
@@ -6,5 +6,5 @@ require('dap-cs').setup()
 dap.listeners.before['event_initialized']['build_before_debug'] = function(session, body)
     print("Building project before debugging...")
     local result = vim.fn.system("dotnet build -c Debug")
-    print(result)
+--    print(result)
 end
