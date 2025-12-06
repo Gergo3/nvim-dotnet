fdec5a8ec5f234ea2cd0b23f4b63283a9506a9c5
Simon Gergely <simon.gergely2006@gmail.com>
2025-12-06 18:52:19 +0100

    Build program before debuging

--- .config/nvim/dap/nvim-dotnet.lua
+++ .config/nvim/dap/nvim-dotnet.lua
@@ -1,2 +1,12 @@
 --debugger setup
-require('dap-cs').setup()
+require('dap-cs').setup(
+    dap_configurations = {
+        {
+            preLaunchTask = function()
+                print("Building project before debugging...")
+                local result = vim.fn.system("dotnet build -c Debug")
+                print(result)
+            end,
+        },
+    }
+)
