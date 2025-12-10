7484d07a42e319a7e5cb668bab33de450ff8bcc4
Simon Gergely <simon.gergely2006@gmail.com>
2025-12-10 17:11:29 +0100

    set csharpls-extended as handler

--- .config/nvim/lsp/nvim-dotnet.lua
+++ .config/nvim/lsp/nvim-dotnet.lua
@@ -4,15 +4,21 @@
 --require("csharpls_extended").buf_read_cmd_bind()
 
 vim.lsp.config.csharp_ls.on_attach = function(client, bufnr)
-    require("csharpls_extended").setup {
-        -- plugin options
-    }
     -- semantic tokens for Neovim 0.11.x
     vim.lsp.semantic_tokens.start(0, vim.lsp.get_clients()[1].id)
-    print("✅ csharpls_extended setup + semantic tokens requested")
 end
 
 --vim.lsp.start(vim.lsp.config.csharp_ls)
+local cslsext = require('csharpls_extended').handler
+vim.lsp.config('csharp_ls', {
+    handlers = {
+        ["textDocument/definition"] = cslsext,
+        ["textDocument/typeDefinition"] = cslsext,
+    },
+
+
+})
+
 vim.lsp.enable('csharp_ls')
 
 
